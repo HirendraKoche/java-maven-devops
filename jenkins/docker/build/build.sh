90ccb8e5d2b405af6dd2795a244302b371951993
hirendra <hirendrakoche1@outlook.com>
2020-06-23 12:23:49 +0530

    Build.sh updated to run docker container

--- jenkins/docker/build/build.sh
+++ jenkins/docker/build/build.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
 
+# Start docker maven container
+
+docker container run \
+       --rm \
+       --name mvn-build \
+       --volume ${pwd}:/usr/src/mymaven \
+       --workdir /usr/src/mymaven \
+       maven:3.6.3-jdk-8 \
+       ls -lrt /usr/src/mymaven
+       

e82eb0a8dff0f4d2d12b72c0953be074caea25fd
hirendra <hirendrakoche1@outlook.com>
2020-06-25 14:03:02 +0530

    1.  jenkins/docker/build/build.sh updated -> --volume-from to --volumes-from

--- jenkins/docker/build/build.sh
+++ jenkins/docker/build/build.sh
@@ -7,6 +7,6 @@ WORKDIR=$PWD
 docker container run \
        --rm \
        --name mvn-build \
-       --volume-from $(docker container ls -q --filter name=_jenkins.1) \
+       --volumes-from $(docker container ls -q --filter name=_jenkins.1) \
        --workdir $PWD \
        maven:3.6.3-jdk-8 "$@"
